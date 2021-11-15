e15e66095bd024159923fd42a40dabeec94de5fe
Zina HADJ ALI <zina.hadjali@orange.com>
2021-11-15 15:16:09 +0100

    Working...

--- java/src/test/java/kata/PasswordTest.java
+++ java/src/test/java/kata/PasswordTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.function.BooleanSupplier;
 
-public class KataShould {
+public class PasswordTest {
 
     @Test
     void returnTrueSizeSup6() {

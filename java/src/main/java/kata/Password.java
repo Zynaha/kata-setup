74d4785e756f487799692200f3d4380554f9051a
Zina HADJ ALI <zina.hadjali@orange.com>
2021-11-15 15:21:10 +0100

    Working...

--- java/src/main/java/kata/Password.java
+++ java/src/main/java/kata/Password.java
@@ -4,8 +4,8 @@ import java.util.function.BooleanSupplier;
 
 public class Password {
 
-    public boolean isSizeSup6(String password) {       
-        return false;
+    public boolean isSizeSup6(String password) {
+        return true;
     }
 
 }

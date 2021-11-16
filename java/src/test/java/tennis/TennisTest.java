06e5cf11ddbed660b8ff357fcde8da2cb5f8b0e8
qxth1985 <amal.benkhalifa@sofrecom.com>
2021-11-16 16:55:27 +0100

    Working...

--- java/src/test/java/tennis/TennisTest.java
+++ java/src/test/java/tennis/TennisTest.java
@@ -96,7 +96,7 @@ public class TennisTest {
 
     @Test
     public void checkAllScoresTennisGame3() {
-        TennisGame3 game = new TennisGame3("player1", "player2");
+        TennisGame3 game = new TennisGame3("player", "player2");
         checkAllScores(game);
     }
 

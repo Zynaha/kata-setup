a96e4ed01049a8da13e348f48abe96eb95ceb774
Zina HADJ ALI <zina.hadjali@orange.com>
2021-11-16 10:01:29 +0100

    Working...

--- java/src/main/java/kata/Password.java
+++ java/src/main/java/kata/Password.java
@@ -2,8 +2,11 @@ package kata;
 
 public class Password {
     
-    public boolean isValid(String password, boolean isAdmin) {
-        isAdmin=true;
+    public Password(boolean isAdmin) {
+      isAdmin=true; 
+    }
+
+    public boolean isValid(String password) {
         return password.matches(".*\\d.*") && password.length()>=6 && password.matches(".*[a-zA-Z].*");
     }
 

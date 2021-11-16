1a8a519fcb5155d5ceb5ab2fb7e43698d6f445ce
Zina HADJ ALI <zina.hadjali@orange.com>
2021-11-16 10:13:32 +0100

    Working...

--- java/src/main/java/kata/Password.java
+++ java/src/main/java/kata/Password.java
@@ -5,12 +5,14 @@ import java.util.function.BooleanSupplier;
 public class Password {
     
     public boolean isValid(String password) {
-        return password.matches(".*\\d.*") && password.length()>=6 && password.matches(".*[a-zA-Z].*");
+        return password.matches(".*\\d.*") && password.length()>6 && password.matches(".*[a-zA-Z].*");
     }
 
     public boolean isAdminValid(String password) {
-        // TODO Auto-generated method stub
-        return null;
+        return password.matches(".*\\d.*") 
+            && password.length()>=10 
+            && password.matches(".*[a-zA-Z].*")
+            && password.matches(".*[!@#$%&*()_+=|<>?{}\\\\[\\\\]~-].*") ;
     }
 
 }

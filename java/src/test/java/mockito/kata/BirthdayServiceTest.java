5e7a7096e8755644c73a2ce057f08e5305bd2c38
qxth1985 <amal.benkhalifa@sofrecom.com>
2021-11-17 12:26:37 +0100

    Working...

--- java/src/test/java/mockito/kata/BirthdayServiceTest.java
+++ java/src/test/java/mockito/kata/BirthdayServiceTest.java
@@ -2,8 +2,29 @@ package mockito.kata;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import mockito.clients.ClientRepository;
+
 public class BirthdayServiceTest {
+ 
   
-  
+@Test
+void test() {
+	//Given
+	ClientRepository repository = Mockito.mock(ClientRepository.class) ;
+	BirthdayService birthdayService=new BirthdayService(repository);
+
+
+	//When 
+	String greeting =birthdayService.greeting("AmaZ*");
+ 
+	//Then 
+	assertThat(greeting.equals("AmaZ*"));
+}
+
+
+
 
 }

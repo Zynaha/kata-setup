583bb1406d3030da0f38628db85940c3ea336cfb
Zina HADJ ALI <zina.hadjali@orange.com>
2021-11-17 10:02:43 +0100

    Working...

--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -53,10 +53,8 @@ public class TennisGame1 implements TennisGame {
             score = getEqualScore(score1);
         } else if (score1 >= 4 || score2 >= 4) {
             int minusResult = score1 - score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            scoresMinusResultMap.getOrDefault(score1,"Win for player2");
+            if (minusResult >= 2) score = "Win for player1";
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) tempScore = score1;

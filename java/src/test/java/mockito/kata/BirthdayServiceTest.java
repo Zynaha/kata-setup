98dbe9f6fbacd2ae324b88150a0d60dcf8df6140
qxth1985 <amal.benkhalifa@sofrecom.com>
2021-11-17 12:42:36 +0100

    Working...

--- java/src/test/java/mockito/kata/BirthdayServiceTest.java
+++ java/src/test/java/mockito/kata/BirthdayServiceTest.java
@@ -33,7 +33,7 @@ public void return_happy_birthday() {
 	
 	//When 
 	String greeting =birthdayService.greeting("AmaZ");
-	Mockito.when(repository.birthdayIsTodayFor("AmaZ")).thenReturn(false);	
+	Mockito.when(repository.birthdayIsTodayFor("AmaZ")).thenReturn(true);	
 	//Then 
 	assertEquals(greeting,"Good morning AmaZ.");
 }

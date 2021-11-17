cc2b7d222c51f61be2996b950f6226898934d36d
qxth1985 <amal.benkhalifa@sofrecom.com>
2021-11-17 12:49:30 +0100

    Working...

---
+++ java/src/test/java/mockito/kata/MaillerBirthdayServiceTest.java
@@ -0,0 +1,44 @@
+package mockito.kata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import mockito.clients.ClientRepository;
+
+public class MaillerBirthdayServiceTest {
+ 
+  
+@Test
+public void by_default_return_good_morning() {
+	//Given
+	ClientRepository repository = Mockito.mock(ClientRepository.class) ;
+	BirthdayService birthdayService=new BirthdayService(repository);
+
+
+	//When 
+	String greeting =birthdayService.greeting("AmaZ");
+ 
+	//Then 
+	assertEquals(greeting,"Good morning AmaZ.");
+}
+@Test
+public void return_happy_birthday() {
+	//Given
+	ClientRepository repository = Mockito.mock(ClientRepository.class) ;
+	BirthdayService birthdayService=new BirthdayService(repository);
+	
+	
+	//When 
+	Mockito.when(repository.birthdayIsTodayFor("AmaZ")).thenReturn(true);	
+	String greeting =birthdayService.greeting("AmaZ");
+	//Then 
+	assertEquals(greeting,"Happy birthday AmaZ!");
+}
+
+
+
+
+}

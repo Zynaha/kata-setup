a42fdf6dbb6aee83a7fc8b842a46868d73dd0cbe
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-06 12:13:49 +0100

    Working...

--- java/src/main/java/org/craftedsw/tripservicekata/Main.java
+++ java/src/main/java/org/craftedsw/tripservicekata/Main.java
@@ -1,6 +1,7 @@
 package org.craftedsw.tripservicekata;
 
 import org.craftedsw.tripservicekata.user.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.UserSession;
 import org.craftedsw.tripservicekata.trip.TripService;
 import org.craftedsw.tripservicekata.user.User;
 

828431bef99806df9577060ef875cd0f72560617
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-06 12:28:38 +0100

    Working...

--- java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -6,18 +6,16 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TripServiceTest {
-	TripService tripService=new TripService();
-	
-    //@Test(expected =UserNotLoggedInException.class )
+	TripService tripService = new TripService();
+
+	// @Test(expected =UserNotLoggedInException.class )
 	@Test
 	public void throw_exception_when_user_not_logged() {
-	  //tripService.getTripsByUser(null, null);
-	  Assertions.assertThrows(UserNotLoggedInException.class, ()->{
-		tripService.getTripsByUser(null, null);
-	});
-    	
+		// tripService.getTripsByUser(null, null);
+		Assertions.assertThrows(UserNotLoggedInException.class, () -> {
+			tripService.getTripsByUser(null, null);
+		});
+
 	}
-    
-    
-    
+
 }

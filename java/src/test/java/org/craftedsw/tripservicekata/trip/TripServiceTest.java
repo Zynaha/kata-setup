cf2d229a3ac77edd750448a0b84c63541f9c26d9
johan.martinsson <johan.martinsson@dexyos.fr>
2021-12-06 14:52:33 +0100

    Working...

--- java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -24,4 +24,37 @@ public class TripServiceTest {
 		tripService.getTripsByUser(traveller, userLogged);
 	}
 
+    TripService service = new TripService();
+
+    @Test
+    void throws_exception_for_guests() throws Exception {
+        Assertions.assertThrows(UserNotLoggedInException.class, () -> service.getTripsByUser(null, null));
+    }
+
+    @Test
+    void no_trips_when_user_has_no_friends() throws Exception {
+        User user1 = new User(); // TODO find good name
+        User loggedUser = new User();
+        List<Trip> result = service.getTripsByUser(user1, loggedUser);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void name() throws Exception {
+        User user1 = new User(); // TODO find good name
+        User loggedUser = new User();
+        user1.addFriend(loggedUser);
+        service = new TripService() { // on pourrait faire ceci avec Mockito.spy
+            @Override
+            protected List<Trip> findTripsBy(User user) {
+                return Arrays.asList(new Trip(), new Trip());
+            }
+        };
+
+
+        List<Trip> result = service.getTripsByUser(user1, loggedUser);
+        assertThat(result).hasSize(2);
+
+    }
+
 }

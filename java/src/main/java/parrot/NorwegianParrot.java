a73bed2695de6693fa56ae502f5a212cbbd33556
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 11:16:35 +0100

    Working...

--- java/src/main/java/parrot/NorwegianParrot.java
+++ java/src/main/java/parrot/NorwegianParrot.java
@@ -1,5 +1,12 @@
 package parrot;
 
 public class NorwegianParrot extends Parrot {
+    private double voltage;
+    private boolean isNailed;
+	public NorwegianParrot(double voltage, boolean isNailed) {
+		super(ParrotTypeEnum.NORWEGIAN_BLUE, 0, voltage, isNailed);
+		this.voltage=voltage;
+		this.isNailed=isNailed;
+	}
 
 }

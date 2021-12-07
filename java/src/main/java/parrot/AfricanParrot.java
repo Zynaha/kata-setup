a901fcefc22b6cf7548f31f04956bcfd5101e0d1
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 11:03:52 +0100

    Working...

--- java/src/main/java/parrot/AfricanParrot.java
+++ java/src/main/java/parrot/AfricanParrot.java
@@ -2,7 +2,7 @@ package parrot;
 
 public class AfricanParrot extends Parrot {
 	
-    private numberOfCoconuts;
+    private int numberOfCoconuts;
 	public AfricanParrot(int numberOfCoconuts) {
 		super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, 0, false);
 	}

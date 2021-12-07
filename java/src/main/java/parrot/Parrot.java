d962026b4023253a77b96ce197111bb8ec772062
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 11:27:08 +0100

    Working...

--- java/src/main/java/parrot/Parrot.java
+++ java/src/main/java/parrot/Parrot.java
@@ -2,11 +2,6 @@ package parrot;
 
 public abstract class Parrot {
 
-    private ParrotTypeEnum type;
-    private int numberOfCoconuts;
-    private double voltage;
-    private boolean isNailed;
-
     public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         this.type = type;
         this.numberOfCoconuts = numberOfCoconuts;

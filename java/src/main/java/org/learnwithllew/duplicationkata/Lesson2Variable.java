a2bc4f2311f1747ade474c6e31c976f874e1f6c9
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 15:21:29 +0100

    Working...

--- java/src/main/java/org/learnwithllew/duplicationkata/Lesson2Variable.java
+++ java/src/main/java/org/learnwithllew/duplicationkata/Lesson2Variable.java
@@ -4,18 +4,15 @@ public class Lesson2Variable extends Song
 {
   public void singBottlesOfBeer()
   {
-    sing("100 bottles of beer on the wall");
+    sing("100 bottles of beer on the wall");  
     
-    sing("100 bottles of beer");
-    
+    sing("100 bottles of beer");   
     sing("Take one down, pass it around");
     
     sing("99 bottles of beer on the wall");
-    
     sing("99 bottles of beer on the wall");
     
-    sing("99 bottles of beer");
-    
+    sing("99 bottles of beer");    
     sing("Take one down, pass it around");
     
     sing("98 bottles of beer on the wall");

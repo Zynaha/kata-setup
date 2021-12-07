db092adf342ebaad955f1e56fed72d5c42db0e71
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 11:12:44 +0100

    Working...

--- java/src/main/java/parrot/AfricanParrot.java
+++ java/src/main/java/parrot/AfricanParrot.java
@@ -5,6 +5,7 @@ public class AfricanParrot extends Parrot {
     private int numberOfCoconuts;
 	public AfricanParrot(int numberOfCoconuts) {
 		super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, 0, false);
+		this.numberOfCoconuts=numberOfCoconuts;
 	}
 	public double getSpeed() {
 		return Math.max(0, getBaseSpeed() - getLoadFactor() * this.numberOfCoconuts);

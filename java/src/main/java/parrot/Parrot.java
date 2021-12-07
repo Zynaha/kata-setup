11b4787a34c12b7c3836a7410e5f5242b0c98979
johan.martinsson <johan.martinsson@dexyos.fr>
2021-12-07 14:09:39 +0100

    Working...

--- java/src/main/java/parrot/Parrot.java
+++ java/src/main/java/parrot/Parrot.java
@@ -10,29 +10,3 @@ public abstract class Parrot {
 
 }
 
-public interface IParrot {
-
-    double getSpeed();
-
-}
-
-public class NorwegianParrot_ implements IParrot {
-
-    private final BaseSpeed baseSpeed;
-
-    public NorwegianParrot_() {
-        this.baseSpeed = new BaseSpeed();
-    }
-
-    @Override
-    public double getSpeed() {
-        return /* faire mon calcul*/ 0;
-    }
-}
-
-
-public class BaseSpeed {
-    public double getBaseSpeed() {
-        return 12.0;
-    }
-}

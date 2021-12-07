57001687bb715d85d6a2a147d50996ac75fc6d40
johan.martinsson <johan.martinsson@dexyos.fr>
2021-12-07 14:55:03 +0100

    Working...

--- java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
+++ java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
@@ -2,26 +2,27 @@ package org.learnwithllew.duplicationkata;
 
 
 public class Lesson1Straight extends Song {
-	public void singCatSong() {
-		String[] myStringArray;
-		myStringArray = new String[] { "We gave the cat to a little kid",
+    public void singCatSong() {
+        String[] myStringArray;
+        myStringArray = new String[]{"We gave the cat to a little kid",
 
-                                        "But the cat came back",
-				                       "The very next day",
-				                       "Oh the cat came back",
-				                       "We thought he was a goner",
-				                       "But the cat came back, he just wouldn't go away",
-				                       "We sent the cat out on a boat",
-				                       "But the cat came back",
-				                       "The very next day",
-				                       "Oh the cat came back",
-				                       "We thought he was a goner",
-									   "But the cat came back, he just wouldn't go away"
-				                       
-				                       };
-	
-	for (int j=0;j<myStringArray.length;j++) {
-		sing(myStringArray[j]);
-	}
-	}
+                "But the cat came back",
+                "The very next day",
+                "Oh the cat came back",
+                "We thought he was a goner",
+                "But the cat came back, he just wouldn't go away",
+                "We sent the cat out on a boat",
+
+                "But the cat came back",
+                "The very next day",
+                "Oh the cat came back",
+                "We thought he was a goner",
+                "But the cat came back, he just wouldn't go away"
+
+        };
+
+        for (int j = 0; j < myStringArray.length; j++) {
+            sing(myStringArray[j]);
+        }
+    }
 }

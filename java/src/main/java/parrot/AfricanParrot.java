a02fd5a0e35ba91538957eb4a7cc2d7eab1eeefc
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 10:35:20 +0100

    Working...

--- java/src/main/java/parrot/AfricanParrot.java
+++ java/src/main/java/parrot/AfricanParrot.java
@@ -2,4 +2,9 @@ package parrot;
 
 public class AfricanParrot extends Parrot {
 
+	public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+		super(type, numberOfCoconuts, voltage, isNailed);
+		// TODO Auto-generated constructor stub
+	}
+
 }

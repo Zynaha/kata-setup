2cf7db6b0b80c490e3123a31f5a7aa8f0febac97
johan.martinsson <johan.martinsson@dexyos.fr>
2021-12-07 14:55:17 +0100

    Working...

--- java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
+++ java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
@@ -7,8 +7,8 @@ public class Lesson1Straight extends Song {
         myStringArray = new String[]{"We gave the cat to a little kid",
 
                 "But the cat came back",
-
                 "The very next day",
+                
                 "Oh the cat came back",
                 "We thought he was a goner",
                 "But the cat came back, he just wouldn't go away",
@@ -16,7 +16,7 @@ public class Lesson1Straight extends Song {
 
                 "But the cat came back",
                 "The very next day",
-                
+
                 "Oh the cat came back",
                 "We thought he was a goner",
                 "But the cat came back, he just wouldn't go away"

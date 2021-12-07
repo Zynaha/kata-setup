946b4e74fb7b4bfb53fdc06c07e97c6ba43cc42c
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 14:46:44 +0100

    Working...

--- java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
+++ java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
@@ -5,7 +5,7 @@ public class Lesson1Straight extends Song {
 	public void singCatSong() {
 		String[] myStringArray;
 		myStringArray = new String[] { "We gave the cat to a little kid", "But the cat came back", "The very next day",
-				"Oh the cat came back", "We thought he was a goner", "But the cat came back he just wouldn't go away",
+				"Oh the cat came back", "We thought he was a goner", "But the cat came back, he just wouldn't go away",
 				"We sent the cat out on a boat", "But the cat came back", "The very next day", "Oh the cat came back",
 				"We thought he was a goner", "But the cat came back, he just wouldn't go away" };
 	

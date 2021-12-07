3ef8d5acf7e5755c70cdcfa814cbc5dce32638c1
Zina HADJ ALI <zina.hadjali@orange.com>
2021-12-07 14:35:46 +0100

    Working...

--- java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
+++ java/src/main/java/org/learnwithllew/duplicationkata/Lesson1Straight.java
@@ -8,5 +8,9 @@ public class Lesson1Straight extends Song {
 				"Oh the cat came back", "We thought he was a goner", "But the cat came back he just wouldn't go away",
 				"We sent the cat out on a boat", "But the cat came back", "The very next day", "Oh the cat came back",
 				"We thought he was a goner", "But the cat came back he just wouldn't go away" };
+	
+	for (int j=0;j<myStringArray.length;j++) {
+		sing(myStringArray[j].concat("/n"));
+	}
 	}
 }
